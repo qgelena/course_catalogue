b55943fdfa9b167a1e62aa672c61dd1cf7ac4aed
Alona S <qgelena@gmail.com>
2021-05-04 21:41:32 +0100

    Add `POST /api/course/search` and 2 tests
    Request payload is:
    - either a JSON string, to search this substring in course names
    - or a JSON dict with a combination of the following keys:
      + `coursename`: the substring to search for in course names
      + `startafter`: a date in the format 2021-05-04 to filter courses with later start date
    
    The response is:
    
    - HTTP code 200, a JSON array of course records that match (may be empty if nothing is found).
    - HTTP code 400, `{"error": "..."}` in case of an error.

--- courseapp.py
+++ courseapp.py
@@ -19,8 +19,9 @@ from flask import (
     jsonify
 )
 import models
-from models import db
+from models import db, Course
 import sqlalchemy as sa
+from sqlalchemy import and_
 
 def create_app(sqlitepath='sqlite://'):
     app = Flask(__name__)
@@ -74,7 +75,7 @@ def create_app(sqlitepath='sqlite://'):
     @app.route('/api/course/<int:id>', methods=['DELETE'])
     def course_delete(id):
         
-        course = models.Course.query.get(id)
+        course = Course.query.get(id)
         if course is None:
             return jsonify({'error':'not found', 'course':id}), 404
         db.session.delete(course)
@@ -82,21 +83,45 @@ def create_app(sqlitepath='sqlite://'):
 
         return jsonify({'status':'ok'})
 
+    # Пошук курсу за назвою і фільтр по датах (search)
+    @app.route('/api/course/search', methods=['POST'])
+    def searchcourse():
+        if type(request.json) == str:
+            search = '%' + request.json + '%'
+            search_filter = Course.coursename.like(search)
+        elif type(request.json) == dict:
+            search_filter = sa.true() # '1=1'
+            if 'coursename' in request.json:
+                # TODO: AND in SQLAlchemy
+                search = '%' + request.json['coursename'] +'%'
+                search_filter = Course.coursename.like(search)
+            if 'startafter' in request.json:
+                try:
+                    startafter = request.json['startafter']
+                    startafter = datetime.strptime(startafter, '%Y-%m-%d').date()
+                    search_filter = and_(search_filter, Course.startdate >= startafter)
+                except ValueError as e:
+                    return jsonify({'error': str(e)}), 400
+        else:
+            return jsonify({'error': 'invalid search format'}), 400
+
+        q = Course.query.filter(search_filter)
+        #print('SQL:', str(q))
+        results = q.all()
+        return jsonify(results)
+
+    # Зміна атрибутів курсу (update)
+    @app.route('/api/course/<id>', methods=['PATCH'])
+    def coursechange():
+        pass
+
+
     db.init_app(app)
 
     with app.app_context():
         db.create_all()
     return app
 
-    # Пошук курсу за назвою і фільтр по датах (search)
-    @app.route('/api/searchcourse', methods=['GET'])
-    def searchcourse():
-        pass
-
-
-    # Зміна атрибутів курсу (update)
-    #@app.route('/api/course/<id>', methods=['PATCH'])
-     
 
 if __name__ == '__main__':
     basedir = Path(__file__).absolute().parent
--- test_app.py
+++ test_app.py
@@ -13,6 +13,34 @@ COURSE_WEAVING = {
     'numberlectures': 42
 }
 
+SNOW_ZORBING = {
+    'coursename': 'snow zorbing',
+    'startdate': '2021-01-01',
+    'finishdate': '2021-02-15',
+    'numberlectures': 15
+}
+
+HILL_ZORBING = {
+    'coursename': 'hill zorbing',
+    'startdate': '2021-03-01',
+    'finishdate': '2021-05-01',
+    'numberlectures': 12
+}
+
+UNDERWATER_EXTREME_IRONING = {
+    'coursename': 'underwater extreme ironing',
+    'startdate': '2021-06-01',
+    'finishdate': '2021-07-17',
+    'numberlectures': 20
+}
+
+PARACHUTE_EXTREME_IRONING = {
+    'coursename': 'parachute extreme ironing',
+    'startdate': '2021-06-01',
+    'finishdate': '2021-08-31',
+    'numberlectures': 34
+}
+
 @pytest.fixture
 def client():
     app = courseapp.create_app()
@@ -54,7 +82,6 @@ def test_info(client):
     # get its info back
     resp = client.get('/api/course/' + str(course_id))
     assert resp.status_code == 200
-    print(resp.json)
     assert resp.json['coursename'] == COURSE_WEAVING['coursename']
     assert resp.json['numberlectures'] == COURSE_WEAVING['numberlectures']
 
@@ -68,6 +95,38 @@ def test_delete(client):
 
     resp = client.delete(f'/api/course/{course_id}')
     assert resp.status_code == 404
+
+def test_search_by_name(client):
+    assert client.post('/api/course', json=COURSE_WEAVING).status_code == 200
+    assert client.post('/api/course', json=HILL_ZORBING).status_code == 200
+    assert client.post('/api/course', json=SNOW_ZORBING).status_code == 200
+    assert client.post('/api/course', json=PARACHUTE_EXTREME_IRONING).status_code == 200
+    assert client.post('/api/course', json=UNDERWATER_EXTREME_IRONING).status_code == 200
+
+    resp = client.post('/api/course/search', json='zorbing')
+    assert resp.status_code == 200
+    assert len(resp.json) == 2
+    courses = [c['coursename'] for c in resp.json]
+    assert sorted(courses) == ['hill zorbing', 'snow zorbing']
+
+def test_search_with_date(client):
+    assert client.post('/api/course', json=COURSE_WEAVING).status_code == 200
+    assert client.post('/api/course', json=HILL_ZORBING).status_code == 200
+    assert client.post('/api/course', json=SNOW_ZORBING).status_code == 200
+    assert client.post('/api/course', json=PARACHUTE_EXTREME_IRONING).status_code == 200
+    assert client.post('/api/course', json=UNDERWATER_EXTREME_IRONING).status_code == 200
+
+    resp = client.post('/api/course/search', json={
+        'coursename':'underwater',
+        'startafter': '2021-05-10'
+    })
+    assert resp.status_code == 200
+    assert len(resp.json) == 1
+    course = resp.json[0]
+    assert course['coursename'] == 'underwater extreme ironing'
+
+def test_change():
+    pass
 
 if __name__ == '__main__':
     pass
